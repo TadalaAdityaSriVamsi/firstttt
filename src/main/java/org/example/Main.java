d4d069884912e2925503502c13e9ae5c5b82b718
Your Name <you@example.com>
2024-12-31 19:43:26 +0530

    Added junit testcases

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        int sum=0;
-        for(int i=1;i<=5;i++){
-            sum+=i;
+        int sum = calculateSum(5);
+        System.out.println("sum: " + sum);
+    }
+
+    public static int calculateSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
         }
-        System.out.println("sum:"+ sum);
+        return sum;
     }
 }
--- src/main/java/org/example/Tadala.java
+++ src/main/java/org/example/Tadala.java
@@ -1,2 +1,35 @@
-package org.example;public class Tadala {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Tadala {
+    public static int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> merged = new ArrayList<>();
+        int[] current = intervals[0];
+        merged.add(current);
+
+        for (int[] interval : intervals) {
+            if (current[1] >= interval[0]) {
+                current[1] = Math.max(current[1], interval[1]);
+            } else {
+                current = interval;
+                merged.add(current);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] result = merge(intervals);
+
+        for (int[] interval : result) {
+            System.out.println(Arrays.toString(interval));
+        }
+    }
 }
+
